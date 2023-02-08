248fd1bb0810991991d584251cfd5e89e8570937
Akd772 <124115995+Akd772@users.noreply.github.com>
2023-02-08 11:06:46 +0530

    Create Octaltodecimal.c

---
+++ Octaltodecimal.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+ int oct;
+ int n = 0;
+ int sum = 0;
+ int a,c,l;
+ printf("Enter an octal number\n");
+ scanf("%d",&oct);
+ l = oct;
+ clrscr();
+ while(oct != 0)
+ {
+  a = oct%10;
+  c = pow(8,n);
+  sum = sum + (a*c);
+  n = n + 1;
+  oct = oct/10;
+ }
+
+ printf("Octal number :%d\n",l);
+ printf("Decimal number :%d\n",sum);
+}
+
